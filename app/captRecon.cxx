3f0e9d63b319e8c9507937f8e9ddd93519a7ee84
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-06-25 09:23:56 -0400

    Update the main program to use TCaptainRecon.

--- app/captRecon.cxx
+++ app/captRecon.cxx
@@ -1,13 +1,13 @@
-#include "TCluster3D.hxx"
+#include "TCaptainRecon.hxx"
 
 #include <eventLoop.hxx>
+
+#include <memory>
 
 /// Run the cluster calibration on a file.
 class TCaptReconLoop: public CP::TEventLoopFunction {
 public:
-    TCaptReconLoop() {
-        fCaptRecon = NULL;
-    }
+    TCaptReconLoop() {}
 
     virtual ~TCaptReconLoop() {};
 
@@ -19,21 +19,22 @@ public:
 
     bool operator () (CP::TEvent& event) {
         // Make sure the electronics simulated is created.
-        if (!fCaptRecon) fCaptRecon = new CP::TCluster3D();
-
         CP::THandle<CP::THitSelection> drift(event.GetHitSelection("drift"));
+        CP::THandle<CP::THitSelection> pmt(event.GetHitSelection("pmt"));
 
         // Run the simulation on the event.
-        CP::THandle<CP::TAlgorithmResult> cluster3D
-            = fCaptRecon->Process(*drift);
-        event.AddFit(cluster3D);
+        std::auto_ptr<CP::TAlgorithm> captRecon(new CP::TCaptainRecon());
+        CP::THandle<CP::TAlgorithmResult> result 
+            = captRecon->Process(*drift,*pmt);
+        if (result) event.AddFit(result);
+        else {
+            CaptError("No reconstruction result");
+        }
 
         // Save everything.
         return true;
     }
 
-private:
-    CP::TCluster3D* fCaptRecon;
 };
 
 int main(int argc, char **argv) {
