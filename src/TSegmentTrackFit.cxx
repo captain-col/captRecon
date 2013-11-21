df78d85cbaa2d54ab57dc138daaaebd95e4982f8
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-11-21 18:57:21 -0500

    Make the segment fit more quiet.

--- src/TSegmentTrackFit.cxx
+++ src/TSegmentTrackFit.cxx
@@ -23,8 +23,6 @@ CP::TSegmentTrackFit::Apply(CP::THandle<CP::TReconTrack>& input) {
         return CP::THandle<CP::TReconTrack>();
     }
 
-    CaptLog("Start segment fit with " << nodes.size() << " nodes");
-
     /////////////////////////////////////////////////////////////////////
     /// Estimate the state at each node.
     /////////////////////////////////////////////////////////////////////
@@ -34,7 +32,7 @@ CP::TSegmentTrackFit::Apply(CP::THandle<CP::TReconTrack>& input) {
          n != nodes.end(); ++n) {
         // Find the next cluster in the track.
         CP::THandle<CP::TReconCluster> nextCluster;
-        if ((n + 1) != nodes.end()) nextCluster = (*(n+1))->GetState();
+        if ((n + 1) != nodes.end()) nextCluster = (*(n+1))->GetObject();
 
         // Get the state from the node.  It had better be a trackState!.
         CP::THandle<CP::TTrackState> trackState = (*n)->GetState();
@@ -93,61 +91,10 @@ CP::TSegmentTrackFit::Apply(CP::THandle<CP::TReconTrack>& input) {
     CP::THandle<CP::TTrackState> firstNodeState = nodes.front()->GetState();
     *trackFront = *firstNodeState;
 
-    // Now move the front state upstream to the position of the first hit.
-    // Notice that the front state is not at the same location as the first
-    // node.  
-    if (nodes.front()->GetObject()) {
-        // There is an object, and there had *better* be one or something is
-        // going horribly wrong.
-        TVector3 pos = trackFront->GetPosition().Vect();
-        TVector3 dir = trackFront->GetDirection();
-        CP::THandle<CP::THitSelection> hits 
-            = nodes.front()->GetObject()->GetHits();
-        if (!hits) {
-            // There had also better be hits!
-            CaptError("No hits in object!");
-            abort();
-        }
-        double upstream = 0.0;
-        for (CP::THitSelection::iterator h = hits->begin(); 
-             h != hits->end(); ++h) {
-            TVector3 diff = (*h)->GetPosition() - pos;
-            upstream = std::min(upstream, diff*dir);
-        }
-        pos = pos + upstream*dir;
-        trackFront->SetPosition(pos.X(), pos.Y(), pos.Z(),
-                                trackFront->GetPosition().T());
-    }
-
     // Set the back state of the track.
     CP::THandle<CP::TTrackState> trackBack = input->GetBack();
     CP::THandle<CP::TTrackState> lastNodeState = nodes.back()->GetState();
     *trackBack = *lastNodeState;
-
-    // Now move the back state downstream to the position of the last hit.
-    // See the comments for "trackFront".
-    if (nodes.back()->GetObject()) {
-        // There is an object, and there had *better* be one or something is
-        // going horribly wrong.
-        TVector3 pos = trackBack->GetPosition().Vect();
-        TVector3 dir = trackBack->GetDirection();
-        CP::THandle<CP::THitSelection> hits 
-            = nodes.back()->GetObject()->GetHits();
-        if (!hits) {
-            // There had also better be hits!
-            CaptError("No hits in object!");
-            abort();
-        }
-        double downstream = 0.0;
-        for (CP::THitSelection::iterator h = hits->begin(); 
-             h != hits->end(); ++h) {
-            TVector3 diff = (*h)->GetPosition() - pos;
-            downstream = std::max(downstream, diff*dir);
-        }
-        pos = pos + downstream*dir;
-        trackBack->SetPosition(pos.X(), pos.Y(), pos.Z(),
-                               trackBack->GetPosition().T());
-    }
 
     int trackDOF = 3*nodes.size() - 6;
     input->SetStatus(TReconBase::kSuccess);
