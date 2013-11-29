11575521cd1811330cff900e1e2f86f46568a117
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-11-29 17:14:11 -0500

    Add a base class for captRecon exceptions.

---
+++ src/ECaptRecon.hxx
@@ -0,0 +1,10 @@
+#ifndef ECaptRecon_hxx_seen
+#define ECaptRecon_hxx_seen
+
+#include <ECore.hxx>
+
+namespace CP {
+    /// Generate a base exception for the Analysis library
+    EXCEPTION(ECaptRecon,ECore);
+};
+#endif
