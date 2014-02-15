45846294c0ee8a4ac9f30327acc24b5f3c09b0a3
Clark McGrew <clark.mcgrew@stonybrook.edu>
2014-02-14 20:44:05 -0500

    Base the TSegmentTrackFit direction covariance off of the moments.

--- src/TSegmentTrackFit.cxx
+++ src/TSegmentTrackFit.cxx
@@ -73,8 +73,12 @@ CP::TSegmentTrackFit::Apply(CP::THandle<CP::TReconTrack>& input) {
         CP::THandle<CP::TClusterState> lastState = last->GetState();
         for (int i=0; i<3; ++i) {
             for (int j=0; j<3; ++j) {
+#ifdef USE_POSITION_COVARIANCE_FOR_DIR_COVARIANCE
                 double v = firstState->GetPositionCovariance(i,j)
                     + lastState->GetPositionCovariance(i,j);
+#else
+                double v = first->GetMoments()(i,j) + last->GetMoments()(i,j);
+#endif
                 v = v/length2;
                 dirCov(i,j) = v;
             }
@@ -102,17 +106,61 @@ CP::TSegmentTrackFit::Apply(CP::THandle<CP::TReconTrack>& input) {
     }
 
     // Set the front state of the track.
-    CP::THandle<CP::TTrackState> trackFront = input->GetFront();
+    CP::THandle<CP::TTrackState> frontState = input->GetFront();
     CP::THandle<CP::TTrackState> firstNodeState = nodes.front()->GetState();
-    *trackFront = *firstNodeState;
-    trackFront->SetEDeposit(energyDeposit);
+    *frontState = *firstNodeState;
+    frontState->SetEDeposit(energyDeposit);
+
+    // Notice that the front state is not at the same location as the first
+    // node.  Adjust the position of the front of the track to be at the
+    // beginning of the first nodes object.
+    TVector3 frontPos = frontState->GetPosition().Vect();
+    TVector3 frontDir = frontState->GetDirection();
+    CP::THandle<CP::THitSelection> frontHits 
+        = nodes.front()->GetObject()->GetHits();
+    if (!frontHits) {
+        // There had also better be hits!
+        CaptError("No hits in object!");
+        abort();
+    }
+    double upstream = 0.0;
+    for (CP::THitSelection::iterator h = frontHits->begin(); 
+         h != frontHits->end(); ++h) {
+        TVector3 diff = (*h)->GetPosition() - frontPos;
+        upstream = std::min(upstream, diff*frontDir);
+    }
+    frontPos = frontPos + upstream*frontDir;
+    frontState->SetPosition(frontPos.X(), frontPos.Y(), frontPos.Z(),
+                            frontState->GetPosition().T());
 
     // Set the back state of the track.
-    CP::THandle<CP::TTrackState> trackBack = input->GetBack();
+    CP::THandle<CP::TTrackState> backState = input->GetBack();
     CP::THandle<CP::TTrackState> lastNodeState = nodes.back()->GetState();
-    *trackBack = *lastNodeState;
-    trackBack->SetEDeposit(0.0);
+    *backState = *lastNodeState;
+    backState->SetEDeposit(0.0);
 
+    // Now move the back state downstream to the position of the last hit.
+    // See the comments for "frontState".
+    TVector3 backPos = backState->GetPosition().Vect();
+    TVector3 backDir = backState->GetDirection();
+    CP::THandle<CP::THitSelection> backHits 
+        = nodes.back()->GetObject()->GetHits();
+    if (!backHits) {
+        // There had also better be hits!
+        CaptError("No hits in object!");
+        abort();
+    }
+    double downstream = 0.0;
+    for (CP::THitSelection::iterator h = backHits->begin(); 
+         h != backHits->end(); ++h) {
+        TVector3 diff = (*h)->GetPosition() - backPos;
+        downstream = std::max(downstream, diff*backDir);
+    }
+    backPos = backPos + downstream*backDir;
+    backState->SetPosition(backPos.X(), backPos.Y(), backPos.Z(),
+                           backState->GetPosition().T());
+
+    // Finish up the fit information...
     int trackDOF = 3*nodes.size() - 6;
     input->SetStatus(TReconBase::kSuccess);
     input->SetAlgorithmName("TSegmentTrackFit");
